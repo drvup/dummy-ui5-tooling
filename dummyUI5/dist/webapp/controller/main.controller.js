09dfb2398bae833d949b901dde58bed298c4b519
Cedric <cedric@cedricheisel.de>
2021-05-06 18:39:16 +0200

    solved resources issue as ui5-task-zipper to old

--- dummyUI5/dist/webapp/controller/main.controller.js
+++
@@ -1,1 +0,0 @@
-sap.ui.define(["sap/ui/core/mvc/Controller","sap/ui/model/json/JSONModel","sap/m/MessageToast"],function(e,n,o){"use strict";return e.extend("dummyUI5.controller.main",{onInit:function(){},onListItemPress:function(e){o.show("Opening "+e.getSource().getTitle());var n=e.getSource().getTooltip();window.open(n,"_blank")}})});
